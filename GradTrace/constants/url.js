508ebf35b05b0bb228f65b5a61a44768c0275b71
ryrulobo <ryru.lobo@gmail.com>
2022-10-11 01:18:11 +0700

    add: minor styling and delete home screen

--- GradTrace/constants/url.js
+++ GradTrace/constants/url.js
@@ -1,1 +1,1 @@
-export const url = "https://c54c-182-3-40-118.ap.ngrok.io"; // link dinamis, tolong disesuaikan sama ngrok di masing2 pc (pastikan ngrok tetap running)
+export const url = "https://48fa-111-94-69-71.ap.ngrok.io"; // link dinamis, tolong disesuaikan sama ngrok di masing2 pc (pastikan ngrok tetap running)
