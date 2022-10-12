ff7a2e24b6f0cc41c8950baaa8e24641dc389c33
isoletslicer <ardogible@gmail.com>
2022-10-12 15:57:35 +0700

    Add : Wiring Profile with scores student

--- GradTrace/constants/url.js
+++ GradTrace/constants/url.js
@@ -1,1 +1,1 @@
-export const url = "https://14a4-111-94-112-193.ap.ngrok.io"; // link dinamis, tolong disesuaikan sama ngrok di masing2 pc (pastikan ngrok tetap running)
+export const url = "https://4ce1-140-213-16-161.ap.ngrok.io"; // link dinamis, tolong disesuaikan sama ngrok di masing2 pc (pastikan ngrok tetap running)
